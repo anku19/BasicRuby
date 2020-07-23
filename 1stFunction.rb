510977f3b0d198eba3b007f67d03dd5a5c4e51eb
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 17:40:53 +0530

    Create 1stFunction.rb

---
+++ 1stFunction.rb
@@ -0,0 +1,19 @@
+def print_dots
+	p '.'*34
+end
+
+print_dots
+print_dots
+p "This program prints dots"
+print_dots
+print_dots
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby 1stFunction.rb
+".................................."
+".................................."
+"This program prints dots"
+".................................."
+".................................."
+=end
