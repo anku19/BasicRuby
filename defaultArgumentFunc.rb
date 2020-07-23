82a96902dea3aae120c8975a6157fd32e1e73e8a
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 18:01:08 +0530

    Create defaultArgumentFunc.rb

---
+++ defaultArgumentFunc.rb
@@ -0,0 +1,15 @@
+def print_dots length=30
+	p '.'*length
+end
+
+print_dots
+
+print_dots 60
+
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby defaultArgumentFunc.rb
+".............................."
+"............................................................"
+=end
