a9b200101240553ac45da237719031751ddc852e
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 09:58:18 +0530

    Create until.rb

---
+++ until.rb
@@ -0,0 +1,19 @@
+p "This is until loop"
+
+num= 8
+until num>13 do 
+	p "#{num}"
+	num=num+1
+end
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby until.rb
+"This is until loop"
+"8"
+"9"
+"10"
+"11"
+"12"
+"13"
+=end
