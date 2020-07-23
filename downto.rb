4d1c89291e5889283ac2f796a9d5079cc0047738
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 09:33:44 +0530

    Create downto.rb

---
+++ downto.rb
@@ -0,0 +1,35 @@
+puts "start count from 20 to down"
+
+20.downto 0 do |num|
+ 	p num
+end
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby downto.rb
+start count from 20 to down
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+=end
+
+
+
