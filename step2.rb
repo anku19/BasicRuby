1727e09b461612e240f330bd6c6c50c9c7b1cd40
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 09:51:50 +0530

    Create step2.rb

---
+++ step2.rb
@@ -0,0 +1,14 @@
+p "multiple of 3 in reverse between 9 to 1"
+
+9.step 1, -3 do |num|
+	p "#{num}"
+end
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby step2.rb
+"multiple of 3 in reverse between 9 to 1"
+"9"
+"6"
+"3"
+=end
