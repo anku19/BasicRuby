0c7306f5cb96131ba568ff3a7e4d152d92f15038
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 16:23:01 +0530

    Create NewHash.rb
    this is the short way to describe keys using symbols.

---
+++ NewHash.rb
@@ -0,0 +1,15 @@
+marks = { English: 50, Math: 70, Science: 75 }
+puts "Key => Value"
+marks.each { |a,b|
+puts "#{a} => #{b}"
+}
+
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby newHash.rb
+Key => Value
+English => 50
+Math => 70
+Science => 75
+=end
