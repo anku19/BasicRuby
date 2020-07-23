50a90ecbb030d8d986b57cee939e38d45bc3f1dc
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 09:46:49 +0530

    Create step.rb

---
+++ step.rb
@@ -0,0 +1,12 @@
+puts "stepto is combo of upto and downto"
+
+1.step 8 do |num|
+	print "#{num}, "
+end
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby step.rb
+stepto is combo of upto and downto
+1, 2, 3, 4, 5, 6, 7, 8,
+=end
