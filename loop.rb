410d3dfb5569911944b8c444f9f966016fcadf81
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 10:10:14 +0530

    Create loop.rb

---
+++ loop.rb
@@ -0,0 +1,22 @@
+p "this is infinite loop"
+
+loop do 
+	p " I love ruby"
+end
+
+
+=begin
+Output:
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+I Love Ruby
+......
+=end
