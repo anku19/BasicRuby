720060fafc24f0263fd1839e23d6ccae1f62eb03
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 10:03:14 +0530

    Create break.rb

---
+++ break.rb
@@ -0,0 +1,20 @@
+2.upto 22 do |i|
+break if i == 12
+p "#{i}, "
+end
+
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby break.rb
+"2, "
+"3, "
+"4, "
+"5, "
+"6, "
+"7, "
+"8, "
+"9, "
+"10, "
+"11, "
+=end
