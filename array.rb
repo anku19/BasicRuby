33b3a6dc344d8118cd61769f44591083bc1edf41
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 10:58:32 +0530

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,17 @@
+p "This is my  Ruby array"
+
+my_array =[]              #or my_array=Array.new
+my_array << "Ank"
+my_array << 148
+my_array << Time.now
+
+my_array.each do |element|
+print element 
+end
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby array.rb
+"This is my  Ruby array"
+Ank1482020-07-23 10:55:50 +0530
+=end
