2bb0f883e21573114f990002666c0ba77e8583e8
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 09:42:24 +0530

    Create upto.rb

---
+++ upto.rb
@@ -0,0 +1,20 @@
+puts "upto puts numbers in reverse of downto"
+
+26.upto 34 do |num|
+	p "#{num}"
+end
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby upto.rb
+upto puts numbers in reverse of downto
+"26"
+"27"
+"28"
+"29"
+"30"
+"31"
+"32"
+"33"
+"34"
+=end
