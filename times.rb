5585d167bfc79686a4b3b4d51f97f10b77e2a18b
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-23 09:36:43 +0530

    Create times.rb

---
+++ times.rb
@@ -0,0 +1,20 @@
+puts "I want my lines 9 times using times loop"
+
+9.times{
+	puts "I love Ruby"
+}
+
+=begin
+Output:
+C:\Users\USER\Desktop>ruby times.rb
+I want my lines 9 times using times loop
+I love Ruby
+I love Ruby
+I love Ruby
+I love Ruby
+I love Ruby
+I love Ruby
+I love Ruby
+I love Ruby
+I love Ruby
+=end
