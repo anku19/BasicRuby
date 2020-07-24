46075c3f7d511ee11f769afe90e9f7e2a2b7eb2c
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-24 11:23:48 +0530

    Create keyword_argument_no_defaults.rb

---
+++ keyword_argument_no_defaults.rb
@@ -0,0 +1,15 @@
+def say_hello name:, age:
+puts "Hello #{name} your age is #{age}"
+end
+
+say_hello name: "Joseph", age: 7
+say_hello age: 21, name: "Vignesh"
+
+
+
+=begin
+output:
+C:\Users\USER\Desktop>keyword_argument_no_defaults.rb
+Hello Joseph your age is 7
+Hello Vignesh your age is 21
+=end
