6aa76f19a30aed3202490abcf6ba8f193098d729
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-24 12:22:00 +0530

    Create class.rb

---
+++ class.rb
@@ -0,0 +1,17 @@
+class Rectangle
+	attr_accessor :length
+end
+
+r = Rectangle.new
+
+r.length =5
+
+p "Side length of r = #{r.length}"
+	
+  
+  
+ =begin
+ Output:
+ C:\Users\USER\Desktop>class.rb
+"Side length of r = 5"
+=end
