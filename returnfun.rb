e41f4ba4bb2ffab228d2677d0a041c42862dc4e9
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-24 11:05:48 +0530

    Create returnfun.rb
    We can return function

---
+++ returnfun.rb
@@ -0,0 +1,17 @@
+def multiplication num1, num2
+	mul= num1 * num2
+	return mul
+end
+
+num1, num2 = 5, 76
+
+print "Multiplication of #{num1} and #{num2} is :"
+p multiplication num1,num2
+
+
+=begin
+Output:
+
+C:\Users\USER\Desktop>funreturn.rb
+Multiplication of 5 and 76 is :380
+=end
