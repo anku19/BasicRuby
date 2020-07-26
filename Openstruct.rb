9371d083e07eeae8b42ef1fc64fd833f270abbf1
anku19 <58931161+anku19@users.noreply.github.com>
2020-07-26 22:23:13 +0530

    Create Openstruct.rb

---
+++ Openstruct.rb
@@ -0,0 +1,11 @@
+require 'ostruct'
+p = OpenStruct.new
+p.name = "Ank"
+p.age = 20
+puts "Hello, I am #{p.name}, age #{p.age}"
+
+
+=begin
+C:\Users\USER\Desktop>openstruct.rb
+Hello, I am Ank, age 20
+=end
